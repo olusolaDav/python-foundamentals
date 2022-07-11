10e6dcd265559c22b825076119ef9ab9a557d1c5
Akinbode Olusola <akinbodeolusola9611@gmail.com>
2022-07-11 13:21:17 +0100

    dictionary iteration

---
+++ day1/dictionary.py
@@ -0,0 +1,12 @@
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+
+word_counter = {}
+
+for word in book_title:
+    if word not in word_counter:
+        word_counter[word] = 1
+    else:
+        word_counter[word] += 1
+
+
+print(word_counter)
