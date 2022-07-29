ac896bf9e0fb89e70b7b337c69ccffcb5d089e18
Akinbode Olusola <akinbodeolusola9611@gmail.com>
2022-07-29 16:13:32 +0100

    for loop

--- day1/dictionary.py
+++ day1/dictionary.py
@@ -10,3 +10,35 @@ for word in book_title:
 
 
 print(word_counter)
+
+
+# You would like to count the number of fruits in your basket. 
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits, but you do not want to count the other items in your basket.
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for object, count in basket_items.items():
+   if object in fruits:
+       result += count
+
+#if the key is in the list of fruits, add the value (number of fruits) to result
+
+
+print(result)
+
+
+fruit_count, not_fruit_count = 0, 0
+
+#Iterate through the dictionary
+for object, count in basket_items.items():
+    if object in fruits:
+       fruit_count += count
+    else:
+        not_fruit_count += count
+
+print("The number of fruits is {}.  There are {} objects that are not fruits.".format(fruit_count, not_fruit_count))
